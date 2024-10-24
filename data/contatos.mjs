3b67d4adf28f017808c97ad58a42cf434ca9b496
Dev-Kauan <kauanlima127@gmail.com>
2024-10-23 22:02:35 -0300

    feat: Criando API de contatos e começando a criar uma imagem docker.

---
+++ data/contatos.mjs
@@ -0,0 +1,18 @@
+const contatos = [
+    {
+        id: 1,
+        nome: 'Kauan',
+        genero: 'M',
+        telefone: '123456789',
+        email: 'kauan@gmail.com'
+    },
+    {
+        id: 2,
+        nome: 'Maria',
+        genero: 'F',
+        telefone: '123456789',
+        email: 'maria@gmail.com'
+    }
+];
+
+export default contatos;
